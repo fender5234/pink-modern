5f5eda81d3f816bb8810ab010c56ffb96ba5a87b
Andrey Efimov <efiand@ya.ru>
2022-04-13 20:43:49 +0300

    Новая сборка со стилями

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,76 @@
 import bemValidator from 'gulp-html-bem-validator';
-import browsersync from 'browser-sync';
-import del from 'del';
+import browser from 'browser-sync';
 import gulp from 'gulp';
 import lintspaces from 'gulp-lintspaces';
 import posthtml from 'gulp-posthtml';
 import rename from 'gulp-rename';
+import less from 'gulp-less';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import stylelint from 'stylelint';
+import postcssBemLinter from 'postcss-bem-linter';
+import postcssReporter from 'postcss-reporter';
 
-const { src, dest, watch, series } = gulp;
-const server = browsersync.create();
+const { src, dest, watch, series, parallel } = gulp;
 const checkLintspaces = () => lintspaces({
   editorconfig: '.editorconfig'
 });
+const editorconfigSources = [
+  'source/njk/**/*.njk',
+  '*.json',
+  '*.js',
+  'source/img/**/*.svg'
+];
 
-const buildHTML = () => src('source/njk/pages/**/*.njk')
+export const buildHTML = () => src('source/njk/pages/**/*.njk')
   .pipe(posthtml())
   .pipe(bemValidator())
   .pipe(rename({ extname: '.html' }))
-  .pipe(dest('source'));
+  .pipe(dest('source'))
+  .pipe(browser.stream());
 
-const testHTML = () => src('source/njk/**/*.njk')
+export const testEditorconfig = () => src(editorconfigSources)
   .pipe(checkLintspaces())
   .pipe(lintspaces.reporter());
 
-const reload = (done) => {
-  server.reload();
+export const styles = () => src('source/less/style.less', { sourcemaps: true })
+  .pipe(less())
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+  .pipe(dest('source/css', { sourcemaps: '.' }))
+  .pipe(browser.stream());
+
+export const testStyles = () => src('source/less/**/*.less', { sourcemaps: true })
+  .pipe(checkLintspaces())
+  .pipe(lintspaces.reporter())
+  .pipe(postcss([
+    stylelint(),
+    postcssBemLinter(),
+    postcssReporter({
+      clearAllMessages: true,
+      throwError: false
+    })
+  ]));
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
   done();
 };
 
-const startServer = () => {
-  server.init({
-    cors: true,
-    open: true,
-    server: 'source',
-    ui: false,
-    port: 2000
-  });
-
-  watch('source/njk/**/*.njk', series(testHTML, buildHTML, reload));
+const watcher = () => {
+  watch(editorconfigSources, series(testEditorconfig, buildHTML));
+  watch('source/less/**/*.less', series(testStyles, styles));
 };
 
-const cleanDest = () => del([
-  `source/**/*.html`
-]);
+export const build = parallel(buildHTML, styles);
+export const test = parallel(testEditorconfig, testStyles);
 
-export const build = buildHTML;
-export default series(cleanDest, testHTML, buildHTML, startServer);
+export default series(test, build, server, watcher);
