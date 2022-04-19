befdb25dce69791144d50e687f4a4759da185c69
Andrey Efimov <efiand@ya.ru>
2022-04-19 22:24:32 +0300

    Форма добавления изображения

--- source/js/blocks/add.js
+++ source/js/blocks/add.js
@@ -3,9 +3,12 @@ export default class Add {
     this._containerElement = containerElement;
     this._formElement = containerElement.querySelector('form');
     this._labelElement = this._formElement.querySelector('.add__file-chooser');
-    this._fileELement = this._formElement.querySelector('[type="file"]');
+    this._fileElement = this._formElement.querySelector('[type="file"]');
     this._filtersELement = this._formElement.querySelector('.add__filters');
     this._filterTogglerElements = this._formElement.querySelectorAll('.add__filter-toggler');
+    this._cropElement = this._formElement.querySelector('[name="crop"]');
+    this._pinkElement = this._formElement.querySelector('[name="pink"]');
+    this._grayscaleElement = this._formElement.querySelector('[name="grayscale"]');
     this._resetELement = this._formElement.querySelector('[type="reset"]');
 
     this._defaultBg = this._labelElement.style.backgroundImage;
@@ -15,17 +18,35 @@ export default class Add {
 
   _setListeners() {
     this._clickHandler = this._clickHandler.bind(this);
-    this._changeHandler = this._changeHandler.bind(this);
+    this._changeFileHandler = this._changeFileHandler.bind(this);
+    this._inputCropHandler = this._inputCropHandler.bind(this);
+    this._inputPinkHandler = this._inputPinkHandler.bind(this);
+    this._inputGrayscaleHandler = this._inputGrayscaleHandler.bind(this);
     this._resetHandler = this._resetHandler.bind(this);
 
-    this._fileELement.addEventListener('change', this._changeHandler);
+    this._fileElement.addEventListener('change', this._changeFileHandler);
+    this._cropElement.addEventListener('input', this._inputCropHandler);
+    this._pinkElement.addEventListener('input', this._inputPinkHandler);
+    this._grayscaleElement.addEventListener('input', this._inputGrayscaleHandler);
     this._filtersELement.addEventListener('click', this._clickHandler);
     this._formElement.addEventListener('reset', this._resetHandler);
   }
 
-  _changeHandler() {
-    const [file] = this._fileELement.files;
-    this._labelElement.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+  _changeFileHandler() {
+    const [file] = this._fileElement.files;
+    this._clearFilters(`url(${URL.createObjectURL(file)})`);
+  }
+
+  _inputCropHandler() {
+    this._labelElement.style.setProperty('--size', `${this._cropElement.value}%`);
+  }
+
+  _inputPinkHandler() {
+    this._labelElement.style.setProperty('--pink', this._pinkElement.value / 100);
+  }
+
+  _inputGrayscaleHandler() {
+    this._labelElement.style.setProperty('--grayscale', this._grayscaleElement.value / 100);
   }
 
   _clickHandler(evt) {
@@ -44,7 +65,20 @@ export default class Add {
     });
   }
 
-  _resetHandler() {
-    this._labelElement.style.backgroundImage = this._defaultBg;
+  _clearFilters(image) {
+    this._cropElement.value = 100;
+    this._pinkElement.value = 0;
+    this._grayscaleElement.value = 0;
+
+    this._labelElement.style.setProperty('--image', image);
+    this._labelElement.style.setProperty('--size', '100%');
+    this._labelElement.style.setProperty('--pink', 0);
+    this._labelElement.style.setProperty('--grayscale', 0);
+  }
+
+  _resetHandler(evt) {
+    evt.preventDefault();
+
+    this._clearFilters(this._defaultBg);
   }
 }
