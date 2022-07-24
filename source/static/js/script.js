73ea5efc884286581129ab9cc827272ffda8cbed
Andrey Efimov <efiand@ya.ru>
2022-07-24 23:43:40 +0300

    Клиентский роутинг

--- source/static/js/script.js
+++ source/static/js/script.js
@@ -6,11 +6,52 @@ import Slider from './blocks/slider.js';
 import Viewport from './blocks/viewport.js';
 import { setupBlocks } from './common/util.js';
 
-[
-  ['.add', Add],
-  ['.form', Form],
-  ['.map', Map],
-  ['.page-header', PageHeader],
-  ['.slider', Slider],
-  ['.viewport[id]', Viewport]
-].forEach(setupBlocks);
+const titleElement = document.querySelector('title');
+
+const createApp = () => {
+  [
+    ['.add', Add],
+    ['.form', Form],
+    ['.map', Map],
+    ['.page-header', PageHeader],
+    ['.slider', Slider],
+    ['.viewport[id]', Viewport]
+  ].forEach(setupBlocks);
+
+  document.querySelectorAll('[data-route]').forEach((routerLinkELement) => {
+    routerLinkELement.addEventListener('click', (evt) => {
+      const { href } = evt.currentTarget;
+
+      if (!href.endsWith('.html')) {
+        return;
+      }
+
+      evt.preventDefault();
+      if (href.endsWith(location.pathname)) {
+        return;
+      }
+
+      fetch(href)
+        .then((res) => {
+          if (!res.ok) {
+            location = href;
+          }
+
+          return res.text();
+        })
+        .then((html) => {
+          const [, template] = html.match(/<body>(.*)<\/body>/s);
+          const [, title] = html.match(/<title>(.*)<\/title>/s);
+          history.pushState(template, title, href);
+          document.body.innerHTML = template;
+          titleElement.innerHTML = title;
+          createApp();
+        })
+        .catch(() => {
+          location = href;
+        });
+    });
+  });
+};
+
+createApp();
